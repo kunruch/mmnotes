f5db7e6f8fec91619c4d7d96fc8f580db6f7e877
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-09-22 23:50:27 +0530

    Adding firebase

--- app/main.js
+++ app/main.js
@@ -1,5 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueFire from 'vuefire'
+import Firebase from 'firebase'
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA_AwrEDAn-PCacXZKzFOT-bMcpvL0t3GQ",
+  authDomain: "mmnotes-cc952.firebaseapp.com",
+  databaseURL: "https://mmnotes-cc952.firebaseio.com",
+  storageBucket: "mmnotes-cc952.appspot.com",
+  messagingSenderId: "395286631831"
+};
+
+global.firebase = Firebase.initializeApp(config);
+global.db = firebase.database();
+
+Vue.use(VueFire);
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
