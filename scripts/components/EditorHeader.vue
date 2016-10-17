589bcf781561362122c1b612e1239dde7f68c2d5
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-10-17 19:59:46 +0530

    Abstracting out header

--- scripts/components/EditorHeader.vue
+++ scripts/components/EditorHeader.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   header.header
     .container
-      include /html/_includes/_header-branding.pug
+      .header-left
+        include /html/_includes/_header-branding.pug
       div Text | Markdown
-      include /html/_includes/_header-menu.pug
+      .header-right
+        include /html/_includes/_header-menu.pug
 </template>
